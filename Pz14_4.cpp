729b43b2ff468363406db14ddc0b7c8538f9ae8f
Ruslan Denisov <necronlive@mail.ru>
2024-01-20 23:04:44 +0900

    Initial

---
+++ Pz14_4.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::string str = "The Cyrillic alphabet does not work. How can I fix it?";
+
+    std::cout << str << "\n" << str.size() << "\n" << str.substr(0, 1) << "\n" << str.substr(str.length() - 1, 1) << "\n";
+}
